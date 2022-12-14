83edd48aba7bac78c5592a2cbb27d91e35d3f211
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2022-12-14 20:16:34 +0000

    Tried adding vehicle routes to rerouter

--- sumo/gen_routes.py
+++ sumo/gen_routes.py
@@ -3,10 +3,12 @@ import sumolib
 from xml.dom import minidom
 from termcolor import colored
 
-complete_net_file = "./data/porto_clean_laterals.net.xml"   # Netfile with more than the VCI
-route_file = "./data/porto.rou.xml"                         # output
-od_filepath = "./data/porto.od"                             # OD file 
-
+complete_net_path = "./data/porto_clean_laterals.net.xml"   # Netfile with more than the VCI
+route_path = "./data/porto.rou.xml"                         # output
+od_path = "./data/porto.od"                             # OD file 
+reroute_path = "./data/reroute.add.xml"
+mandatory_edge_1 = "405899851"
+mandatory_edge_2 = "478882416"
 
 def gen_root():
     return minidom.Document()
@@ -20,7 +22,50 @@ def gen_vehicle(id : str):
     vehicle.setAttribute("depart", "{:.2f}".format(id))
     return vehicle
 
+def gen_reroute_hash(edges, h):
+    """
+    Creates hashmap containing the rerouter edges.
+    """
+    keys = h.keys()
     
+    for edge in edges: 
+        if edge in keys: 
+            return h
+
+    h[edges[0]] = edges[-1]
+    return h
+
+def gen_reroute(h):
+    doc = gen_root()
+    additionals = doc.createElement("additionals")
+    
+    with open(reroute_path, 'w') as reroute_file:
+        for i, key in enumerate(h.keys()):
+            rerouter = doc.createElement("rerouter")
+            rerouter.setAttribute("id", f"rerouter_{i}")
+            rerouter.setAttribute("edges", f"{key}")
+            interval = doc.createElement("interval")
+            interval.setAttribute("end", "1e6")
+            destProbReroute = doc.createElement("destProbReroute")
+            destProbReroute.setAttribute("id", f"{h[key]}")
+            interval.appendChild(destProbReroute)
+            rerouter.appendChild(interval)
+            additionals.appendChild(rerouter)
+
+            rerouter = doc.createElement("rerouter")
+            rerouter.setAttribute("id", f"rerouter_{i*2+1}")
+            rerouter.setAttribute("edges", f"{h[key]}")
+            interval = doc.createElement("interval")
+            interval.setAttribute("end", "1e6")
+            destProbReroute = doc.createElement("destProbReroute")
+            destProbReroute.setAttribute("id", f"{key}")
+            interval.appendChild(destProbReroute)
+            rerouter.appendChild(interval)
+            additionals.appendChild(rerouter)
+    
+        doc.appendChild(additionals)
+        doc.writexml(reroute_file, indent='\t', addindent='\t', newl='\n', encoding="UTF-8")
+
 def gen_route(net, nodes):
     route_path = ""
     # Get edges from nodes 
@@ -28,7 +73,7 @@ def gen_route(net, nodes):
     outgoing_edge_random = get_random(outgoing_edge)
     incoming_edge = net.getNode(nodes[1]).getIncoming()
     incoming_edge_random = get_random(incoming_edge)
-
+    
     path_edges = net.getFastestPath(outgoing_edge_random, incoming_edge_random)[0]
 
     # Get path 
@@ -37,7 +82,7 @@ def gen_route(net, nodes):
 
     route = doc.createElement("route")
     route.setAttribute("edges", route_path)
-    return route
+    return [route, route_path.split(" ")[:-1]]
 
 def strip_line(line): 
     line = line.replace("\n", "")
@@ -60,7 +105,7 @@ def read_od():
     """
     Read OD FiLe
     """
-    od_lines = open(od_filepath, "r").readlines()
+    od_lines = open(od_path, "r").readlines()
     od = list(map(strip_line, od_lines))
     od = od[find_od_start(od):]
     return list(map(lambda x: x.split(), od))
@@ -69,12 +114,16 @@ def read_od():
 if '__main__' == __name__:
     doc = gen_root()
     routes = doc.createElement("routes")
-    net = sumolib.net.readNet(complete_net_file)
+    net = sumolib.net.readNet(complete_net_path)
     od_file = read_od()
+    # hash to build the circular file
+    hash_reroute = {}
 
     for id in range(500):
-        try: 
-            route = gen_route(net, random.sample(od_file, 1)[0])
+        try:
+            nodes = random.sample(od_file, 1)[0]
+            [route, route_list] = gen_route(net, nodes)
+            hash_reroute = gen_reroute_hash(route_list, hash_reroute)
         except Exception as e: 
             print(colored(f"[ERR] Not possible generate path, skipping...:: {e}", "yellow"))
             continue
@@ -85,5 +134,7 @@ if '__main__' == __name__:
 
     doc.appendChild(routes)
 
-    with open(route_file, "w") as f:
+    gen_reroute(hash_reroute)
+
+    with open(route_path, "w") as f:
         doc.writexml(f, indent='\t', addindent='\t', newl='\n', encoding="UTF-8")
