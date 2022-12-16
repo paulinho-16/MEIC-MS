4a3a299719c48c3da873c1040d2eaf2b7e277523
Paulo Ribeiro <pjsalgadomribeiro@gmail.com>
2022-12-16 10:22:28 +0000

    Fixed reroutes

--- sumo/gen_routes.py
+++ sumo/gen_routes.py
@@ -8,7 +8,7 @@ route_path = "./data/porto.rou.xml"                         # output
 od_path = "./data/porto.od"                             # OD file 
 reroute_path = "./data/reroute.add.xml"
 mandatory_edge_1 = "405899851"
-mandatory_edge_2 = "478882416"
+mandatory_edge_2 = "478882411"
 
 def gen_root():
     return minidom.Document()
@@ -21,60 +21,32 @@ def gen_vehicle(id : str):
     vehicle.setAttribute("id", str(id))
     vehicle.setAttribute("depart", "{:.2f}".format(id))
     return vehicle
-
-def gen_reroute_hash(edges, h):
-    """
-    Creates hashmap containing the rerouter edges.
-    """
-    keys = h.keys()
-    
-    for edge in edges: 
-        if edge in keys: 
-            return h
-
-    h[edges[0]] = edges[-1]
-    return h
-
-def gen_reroute(h):
-    doc = gen_root()
-    additionals = doc.createElement("additionals")
-    
-    with open(reroute_path, 'w') as reroute_file:
-        for i, key in enumerate(h.keys()):
-            rerouter = doc.createElement("rerouter")
-            rerouter.setAttribute("id", f"rerouter_{i}")
-            rerouter.setAttribute("edges", f"{key}")
-            interval = doc.createElement("interval")
-            interval.setAttribute("end", "1e6")
-            destProbReroute = doc.createElement("destProbReroute")
-            destProbReroute.setAttribute("id", f"{h[key]}")
-            interval.appendChild(destProbReroute)
-            rerouter.appendChild(interval)
-            additionals.appendChild(rerouter)
-
-            rerouter = doc.createElement("rerouter")
-            rerouter.setAttribute("id", f"rerouter_{i*2+1}")
-            rerouter.setAttribute("edges", f"{h[key]}")
-            interval = doc.createElement("interval")
-            interval.setAttribute("end", "1e6")
-            destProbReroute = doc.createElement("destProbReroute")
-            destProbReroute.setAttribute("id", f"{key}")
-            interval.appendChild(destProbReroute)
-            rerouter.appendChild(interval)
-            additionals.appendChild(rerouter)
-    
-        doc.appendChild(additionals)
-        doc.writexml(reroute_file, indent='\t', addindent='\t', newl='\n', encoding="UTF-8")
+        
+def create_rerouter_element(origin, destination, index, additionals): 
+    rerouter = doc.createElement("rerouter")
+    rerouter.setAttribute("id", f"rerouter_{index}")
+    rerouter.setAttribute("edges", f"{origin}")
+    interval = doc.createElement("interval")
+    interval.setAttribute("end", "1e6")
+    destProbReroute = doc.createElement("destProbReroute")
+    destProbReroute.setAttribute("id", f"{destination}")
+    interval.appendChild(destProbReroute)
+    rerouter.appendChild(interval)
+    additionals.appendChild(rerouter)
 
 def gen_route(net, nodes):
     route_path = ""
+    
     # Get edges from nodes 
     outgoing_edge = net.getNode(nodes[0]).getOutgoing()
     outgoing_edge_random = get_random(outgoing_edge)
     incoming_edge = net.getNode(nodes[1]).getIncoming()
     incoming_edge_random = get_random(incoming_edge)
     
-    path_edges = net.getFastestPath(outgoing_edge_random, incoming_edge_random)[0]
+    mandatory_edge = net.getEdge(mandatory_edge_1) if random.random() > 0.5 else net.getEdge(mandatory_edge_2)
+
+    path_edges = net.getShortestPath(outgoing_edge_random, mandatory_edge)[0]
+    path_edges += net.getShortestPath(mandatory_edge, incoming_edge_random)[0][1:]
 
     # Get path 
     for edge in path_edges:
@@ -123,7 +95,6 @@ if '__main__' == __name__:
         try:
             nodes = random.sample(od_file, 1)[0]
             [route, route_list] = gen_route(net, nodes)
-            hash_reroute = gen_reroute_hash(route_list, hash_reroute)
         except Exception as e: 
             print(colored(f"[ERR] Not possible generate path, skipping...:: {e}", "yellow"))
             continue
@@ -133,8 +104,6 @@ if '__main__' == __name__:
         routes.appendChild(vehicle)
 
     doc.appendChild(routes)
-
-    gen_reroute(hash_reroute)
 
     with open(route_path, "w") as f:
         doc.writexml(f, indent='\t', addindent='\t', newl='\n', encoding="UTF-8")
