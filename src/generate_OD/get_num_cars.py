7aec3b33c22a525190f29d96a3c605dec67efdb8
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2023-01-11 10:56:31 +0000

    add: file to get create the matrix

---
+++ src/generate_OD/get_num_cars.py
@@ -0,0 +1,5 @@
+"""This file is responsible for generating an OD file, based on the number of cars of real data. 
+For one sensor that has a flow x, this flow is splitted among the pairs (origin, destination) equally. 
+"""
+
+
